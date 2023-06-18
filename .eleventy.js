f6bd41bcdfc418f29761516f8791b5d68c04276a
Brett Jones <contact.waycoolapps@gmail.com>
2023-06-18 02:33:37 -0500

    work on website

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig){
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
